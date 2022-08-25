85a2368857bdd19fc7302cb640179d35bc88e3e6
IsolatedLun <herojoe210@gmail.com>
2022-08-25 14:36:59 +0300

    Integrated frontend into django

--- ecoBackend/ecoBackend/urls.py
+++ ecoBackend/ecoBackend/urls.py
@@ -4,13 +4,22 @@ ecoBackend URL Configuration
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf.urls.static import static
+from django.shortcuts import render
 from django.conf import settings
-from django.views.generic import TemplateView
+
+
+def render_react(request):
+    return render(request, "index.html")
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/products/', include('products.urls')),
+    re_path(r"^$", render_react),
+    re_path(r"^(?:.*)/?$", render_react),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 admin.site.site_header = 'EcoDistributors'
 admin.site.site_title = 'EcoDistributors'
--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -6,7 +6,12 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: adapter({ out: 'dist' })
+		adapter: adapter({
+			assets: 'build/assets',
+			out: 'build',
+			fallback: 'index.html',
+			precompress: false
+		})
 	}
 };
 
